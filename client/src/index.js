534876d715c44e5a584ba280b524d13ac4ecb247
tchoutdara <monkeymilitia444@gmail.com>
2018-09-19 16:57:48 -0600

    route in App.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, 
+document.getElementById('root')
+);
 registerServiceWorker();
---
+++ client/src/reducers/posts.js
@@ -0,0 +1,53 @@
+import axios from 'axios'
+
+const POSTS = 'POSTS'
+const ADD_POST = 'ADD_POST'
+const UPDATE_POST = 'UPDATE_POST'
+const DELETE_POST = 'DELETE_POST'
+
+export const getPosts = () => {
+    return (dispatch) => {
+        axios.get('/api/posts')
+        .then( res => dispatch({ type: POSTS, posts: res.data }) )
+    }
+}
+
+export const addPost = (post) => {
+    return (dispatch) => {
+        axios.post('/api/posts', { post })
+        .then( res => dispatch({ type: ADD_POST, post: res.data}) )
+    }
+}
+
+export const updatePost = (post) => {
+    return (dispatch) => {
+        axios.put(`/api/posts/${id}`)
+        .then( res => dispatch({ type: UPDATE_POST, post: res.data}) )
+    }
+}
+
+export const deletePost = (id) => {
+    return (dispatch) => {
+        axios.delete(`/api/posts/${id}`)
+        .then( () => dispatch({ type: DELETE_POST, id }) )
+    }
+}
+
+export default (state = [], action ) => {
+    switch(action.type) {
+        case POSTS:
+          return action.posts
+        case ADD_POST:
+          return [action.post, ...state]
+        case UPDATE_POST:
+          return state.map( p => {
+              if (p.id === action.post.id)
+                return action.post
+              return p
+          })
+        case DELETE_POST:
+          return state.filter( p => p.id !== action.id)
+        default:
+          return state
+    }
+}
